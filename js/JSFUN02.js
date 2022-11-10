4862d7b3521de4ad2b49a576a1f06002340f48ae
BuildTools <unconfigured@null.spigotmc.org>
2022-11-09 22:55:00 -0500

    Test001

---
+++ js/JSFUN02.js
@@ -0,0 +1,57 @@
+//Encontrar elementos de HTML
+
+const titulo = document.getElementById("titulo");
+const parrafo = document.getElementsByTagName("p");
+//es mejor trabajar con IDs
+//const parrafo = document.getElementById("parrf")
+const linku = document.getElementsByClassName("linku");
+
+console.log(parrafo);
+
+
+//Cambiar valores de HTML
+titulo.innerHTML = "Test de titulo cambiadoo siiiuuuu";
+
+document.getElementById("demo").innerHTML = "el texto en el parrafiño (index 0) es: " + parrafo[0].innerHTML;
+
+linku[0].href = "https://www.facebook.com"; 
+linku[0].target = "_blank";
+
+console.log(linku);
+
+//Cambiar estilos
+
+titulo.style.color = "red";
+
+//Eventos
+
+function cambiarTexto(obj) {
+    obj.innerHTML = "Que pendejo si clickeo lol"
+}
+
+function mOver(obj) {
+    obj.innerHTML = "Yamete kudasaiii"
+}
+
+function mOut(obj) {
+    obj.innerHTML = "AAAhhhhhh"
+}
+
+function mDown(obj) {
+  obj.style.backgroundColor = "#1ec5e5";
+  obj.innerHTML = "sueltame";
+}
+
+function mUp(obj) {
+  obj.style.backgroundColor="#D94A38";
+  obj.innerHTML="gracias";
+}
+
+//addEventListener
+
+const boton = document.getElementById("boton");
+boton.addEventListener("click", alerta)
+
+function alerta() {
+    alert("alerta");
+}
